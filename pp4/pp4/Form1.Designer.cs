afe47846939121f672b26b3e119d52ddd9b36426
kithema <k1th3ma@gmail.com>
2026-03-06 14:04:10 +0300

    Add files via upload

---
+++ pp4/pp4/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace pp4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtPurchaseAmount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblResult;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtPurchaseAmount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сумма покупки (руб):";
+
+           
+            this.txtPurchaseAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtPurchaseAmount.Location = new System.Drawing.Point(180, 27);
+            this.txtPurchaseAmount.Name = "txtPurchaseAmount";
+            this.txtPurchaseAmount.Size = new System.Drawing.Size(150, 23);
+            this.txtPurchaseAmount.TabIndex = 1;
+
+            
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCalculate.Location = new System.Drawing.Point(30, 70);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(140, 35);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Рассчитать скидку";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+
+           
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnClear.Location = new System.Drawing.Point(190, 70);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(140, 35);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold);
+            this.lblResult.Location = new System.Drawing.Point(30, 120);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(260, 18);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Результат будет отображен здесь";
+
+          
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtPurchaseAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Калькулятор скидок";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ pp4/pp4/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace pp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtPurchaseAmount.Text))
+                {
+                    MessageBox.Show("Введите сумму покупки!", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!decimal.TryParse(txtPurchaseAmount.Text, out decimal purchaseAmount) || purchaseAmount < 0)
+                {
+                    MessageBox.Show("Введите корректную положительную сумму!", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                decimal finalAmount = CalculateDiscount(purchaseAmount);
+                decimal discountAmount = purchaseAmount - finalAmount;
+                int discountPercent = GetDiscountPercent(purchaseAmount);
+
+                lblResult.Text = $"Сумма покупки: {purchaseAmount:C}\n" +
+                                $"Скидка: {discountPercent}%\n" +
+                                $"Сумма скидки: {discountAmount:C}\n" +
+                                $"Итоговая сумма: {finalAmount:C}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private decimal CalculateDiscount(decimal amount)
+        {
+            if (amount < 1000)
+                return amount;
+            else if (amount >= 1000 && amount <= 5000)
+                return amount * 0.95m; // 5% скидка
+            else
+                return amount * 0.90m; // 10% скидка
+        }
+
+        private int GetDiscountPercent(decimal amount)
+        {
+            if (amount < 1000)
+                return 0;
+            else if (amount >= 1000 && amount <= 5000)
+                return 5;
+            else
+                return 10;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPurchaseAmount.Clear();
+            lblResult.Text = "Результат будет отображен здесь";
+            txtPurchaseAmount.Focus();
+        }
+    }
+}
